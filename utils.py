7711ac1facea9b1901e7b4a50b2985373d3abd84
sabughazal <sultan.abughazal@tii.ae>
2024-07-03 18:52:32 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,16 +8,18 @@ def save_checkpoint(model, epoch, dirpath, fname="checkpoint.pt"):
         'model_state_dict': model.state_dict(),
     }, os.path.join(dirpath, fname))
 
-# def load_checkpoint(model, device, fname="checkpoint.pt"):
-#     checkpoint = torch.load(os.path.join(".", fname), map_location=device)
-#     model.load_state_dict(checkpoint['model_state_dict'])
-#     return model
+def load_checkpoint(model, device, dirpath, fname="checkpoint.pt"):
+    checkpoint = torch.load(os.path.join(dirpath, fname), map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    return model
 
 def log_model_arch(model, dirpath, fname="model.txt"):
     with open(os.path.join(dirpath, fname), 'w') as f:
         print(model, file=f)
 
-def log_run_args(args, dirpath, fname="args.txt"):
+def log_run_args(args, dirpath, fname="args.json"):
     with open(os.path.join(dirpath, fname), 'w') as f:
         f.write(json.dumps(vars(args), indent=4))
 
+def count_model_params(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
